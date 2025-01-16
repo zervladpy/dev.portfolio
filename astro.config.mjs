bf7edfd5b35567dacded8ea0b2c9430f1881befb
Vladislav Zernov <zernovvladislav7@gmail.com>
2025-01-16 12:48:29 +0100

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+import node from '@astrojs/node';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+
+  adapter: node({
+    mode: 'standalone'
+  })
+});
